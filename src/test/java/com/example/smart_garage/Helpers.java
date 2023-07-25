377d10edbc677fe7d3cf1d5395d06e6bc269b3af
Mariana <mariana_milcheva@abv.bg>
2023-07-25 13:08:29 +0300

    Add Mockito and BrandService unit tests

--- src/test/java/com/example/smart_garage/Helpers.java
+++ src/test/java/com/example/smart_garage/Helpers.java
@@ -1,6 +1,10 @@
 package com.example.smart_garage;
 
 import com.example.smart_garage.models.Brand;
+import com.example.smart_garage.models.Role;
+import com.example.smart_garage.models.User;
+
+import java.util.UUID;
 
 public class Helpers {
 
@@ -9,5 +13,30 @@ public class Helpers {
         brand.setBrandId(1L);
         brand.setBrandName("Tesla");
         return brand;
+    }
+
+    public static User createMockUser() {
+        var mockUser = new User();
+        mockUser.setUserId(1L);
+        mockUser.setUsername("MockUsername");
+        mockUser.setPassword("MockPassword");
+        mockUser.setLastName("MockLastName");
+        mockUser.setFirstName("MockFirstName");
+        mockUser.setPhoneNumber("0890123456");
+        mockUser.setEmail("mock@user.com");
+        mockUser.setRole(new Role(1L, "user"));
+        return mockUser;
+    }
+    public static User createMockUserAdmin() {
+        var mockUser = new User();
+        mockUser.setUserId(1L);
+        mockUser.setUsername("MockAdminUsername");
+        mockUser.setPassword("MockAdminPassword");
+        mockUser.setLastName("MockAdminLastName");
+        mockUser.setFirstName("MockAdminFirstName");
+        mockUser.setPhoneNumber("0890123456");
+        mockUser.setEmail("mockAdmin@user.com");
+        mockUser.setRole(new Role(1L, "admin"));
+        return mockUser;
     }
 }
--- src/test/java/com/example/smart_garage/services/BrandServiceImplTest.java
+++ src/test/java/com/example/smart_garage/services/BrandServiceImplTest.java
@@ -1,37 +1,90 @@
 package com.example.smart_garage.services;
 
+import com.example.smart_garage.models.Brand;
+import com.example.smart_garage.repositories.contracts.BrandRepository;
+import com.example.smart_garage.repositories.contracts.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+import static com.example.smart_garage.Helpers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
 class BrandServiceImplTest {
+    @Mock
+    private BrandRepository brandRepository;
+
+    @InjectMocks
+    private BrandServiceImpl brandService;
 
     @Test
-    void getBrandById() {
+    void getBrandById_Should_ReturnObject() {
+
+        Brand brand = createMockBrand();
+
+        Mockito.when(brandRepository.getById(brand.getBrandId())).thenReturn(brand);
+
+        Optional<Brand> result = Optional.ofNullable(brandService.getBrandById(brand.getBrandId()));
+
+        assertTrue(result.isPresent());
+        assertEquals(brand.getBrandId(), result.get().getBrandId());
+        Mockito.verify(brandRepository, Mockito.times(1)).getById(brand.getBrandId());
     }
 
     @Test
-    void getBrandByName() {
+    void getBrandByName_Should_ReturnObject() {
+        Brand brand = createMockBrand();
+
+        Mockito.when(brandRepository.getByField("brandName", brand.getBrandName())).thenReturn(brand);
+
+        Optional<Brand> result = Optional.ofNullable(brandService.getBrandByName(brand.getBrandName()));
+
+        assertTrue(result.isPresent());
+        assertEquals(brand.getBrandName(), result.get().getBrandName());
+        Mockito.verify(brandRepository, Mockito.times(1)).getByField("brandName", brand.getBrandName());
     }
 
     @Test
-    void getAllBrands() {
+    void getAllBrands_Should_ReturnListOfObjects() {
+        List<Brand> brandList = new ArrayList<>();
+
+        brandList.add(createMockBrand());
+        brandList.add(createMockBrand());
+
+        Optional<String> search = Optional.empty();
+        Mockito.when(brandRepository.getAllBrands(search)).thenReturn(brandList);
+        List<Brand> result = brandService.getAllBrands(search);
+
+        assertEquals(brandList.size(), result.size());
+        Mockito.verify(brandRepository, Mockito.times(1)).getAllBrands(search);
+
+
     }
 
     @Test
-    void getAllBrandOptions() {
+    void deleteBrand_Should_DeleteObject() {
+        Brand brand = createMockBrand();
+
+        brandService.deleteBrand(brand.getBrandId(), createMockUserAdmin());
+        Mockito.verify(brandRepository).delete(brand.getBrandId());
     }
 
     @Test
-    void getAllBrandsFilter() {
-    }
+    void createBrand_Should_ReturnCreatedObject() {
+        Brand brand = createMockBrand();
 
-    @Test
-    void deleteBrand() {
-    }
-
-    @Test
-    void createBrand() {
+        brandService.createBrand(brand);
+        Mockito.verify(brandRepository).create(brand);
     }
 
     @Test
